842bef37c704df00f5e7177d4041d9851bd35034
RABE_IT Services <development@berndrabe.de>
2017-01-21 17:22:05 +0100

    Initial Commit

---
+++ IBAN/IBAN.h
@@ -0,0 +1,19 @@
+//
+//  IBAN.h
+//  IBAN
+//
+//  Created by Bernd Rabe on 21.01.17.
+//  Copyright © 2017 RABE_IT Services. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IBAN.
+FOUNDATION_EXPORT double IBANVersionNumber;
+
+//! Project version string for IBAN.
+FOUNDATION_EXPORT const unsigned char IBANVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IBAN/PublicHeader.h>
+
+
